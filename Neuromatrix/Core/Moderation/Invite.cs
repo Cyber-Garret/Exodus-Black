b2db017c13bc583b0ab4e943becd728dd8bcc0f6
Cyber Garret <garret333@gmail.com>
2019-01-05 10:24:30 +0200

    add invite and moderation command

---
+++ Neuromatrix/Core/Moderation/Invite.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Neuromatrix.Core.Moderation
+{
+    public class Invite : ModuleBase<SocketCommandContext>
+    {
+        [Command("invite"), Summary("Создает инвайт для сервера.")]
+        public async Task InviteModule(ulong GuildId)
+        {
+            if (!(Context.User.Id == 316272461291192322))
+            {
+                await Context.Channel.SendMessageAsync(":x: Вы не модератор Нейроматрицы!");
+                return;
+            }
+
+            if (Context.Client.Guilds.Where(x => x.Id == GuildId).Count() < 1)
+            {
+                await Context.Channel.SendMessageAsync(":x: Я не состою в гильдии id=" + GuildId);
+                return;
+            }
+
+            SocketGuild Guild = Context.Client.Guilds.Where(x => x.Id == GuildId).FirstOrDefault();
+            var Invites = await Guild.GetInvitesAsync();
+            if (Invites.Count() < 1)
+            {
+                try
+                {
+                    await Guild.TextChannels.First().CreateInviteAsync();
+                }
+                catch (Exception ex)
+                {
+                    await Context.Channel.SendMessageAsync($":x: Создание инвайта для гильдии {Guild.Name} пошло не так, ошибка: ``{ex.Message}``");
+                    return;
+                }
+            }
+
+            Invites = null;
+            Invites = await Guild.GetInvitesAsync();
+            EmbedBuilder Embed = new EmbedBuilder();
+            Embed.WithAuthor($"Инвайт для гильдии {Guild.Name}", Guild.IconUrl);
+            Embed.WithColor(40, 155, 200);
+            foreach (var Current in Invites)
+                Embed.AddInlineField("Инвайт:", $"[Инвайт]({Current.Url})");
+
+            await Context.Channel.SendMessageAsync("", false, Embed.Build());
+        }
+    }
+}
---
+++ Neuromatrix/Core/Moderation/Moderation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+using Newtonsoft.Json;
+
+using Neuromatrix.Resources.Settings;
+using Neuromatrix.Resources.Datatypes;
+
+namespace Neuromatrix.Core.Moderation
+{
+    public class Moderation : ModuleBase<SocketCommandContext>
+    {
+        [Command("reload"), Summary("Перезагрузка значений из файла settings.json во время работы бота.")]
+        public async Task Reload()
+        {
+            //Checks
+            if (Context.User.Id != StaticSettings.Owner)
+            {
+                await Context.Channel.SendMessageAsync(":x: Только капитан корабля и его подчиненные имею доступ к этой команде!");
+                return;
+            }
+
+            string SettingsLocation = @"Data\Settings.json";
+            if (!File.Exists(SettingsLocation))
+            {
+                await Context.Channel.SendMessageAsync(":x: Не найден файл настроек. Путь по которому было произведено сканирование записан в моей консоли.");
+                Console.WriteLine(SettingsLocation);
+                return;
+            }
+            //Execution
+            string JSON = "";
+            using (var Stream = new FileStream(SettingsLocation, FileMode.Open, FileAccess.Read))
+            using (var ReadSettings = new StreamReader(Stream))
+            {
+                JSON = ReadSettings.ReadToEnd();
+            }
+
+            Settings Settings = JsonConvert.DeserializeObject<Settings>(JSON);
+            //Save data
+            StaticSettings.Token = Settings.token;
+            StaticSettings.Owner = Settings.owner;
+            StaticSettings.Log = Settings.log;
+            StaticSettings.Version = Settings.version;
+            StaticSettings.Banned = Settings.banned;
+
+            await Context.Channel.SendMessageAsync(":white_check_mark: Настройки из файла settings.json были успешно загружены в мою память.");
+        }
+    }
+}
