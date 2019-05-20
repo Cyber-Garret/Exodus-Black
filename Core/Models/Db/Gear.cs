4a8e86f9b7c2e16a89e0e6ddf68339a2e7cee796
CyberGarret <garret333@gmail.com>
2019-05-20 22:44:56 +0300

    WIP: Neira site exotic page

--- Core/Models/Db/Gear.cs
+++ Core/Models/Db/Gear.cs
@@ -2,24 +2,38 @@
 
 namespace Core.Models.Db
 {
-    public class Gear
-    {
-        [Key]
-        public int Id { get; set; }
-        public string Name { get; set; } //Холодное сердце
-        public string Type { get; set; } //Энергетическая лучевая винтовка
-        public string IconUrl { get; set; } //http://159.69.21.188/Icon/Energy/Coldheart.jpg
-        public string ImageUrl { get; set; } //http://159.69.21.188/Image/Energy/Coldheart.jpg
-        public string Description { get; set; } //Новейшие технологии Omolon позволяют использовать жидкое топливо в качестве хладагента, обеспечивая оружейным системам минусовую температуру.
-        public string PerkName { get; set; } //Холодный синтез
-        public string PerkDescription { get; set; } //Это оружие оснащено лазером с термоядерной накачкой, испускающим непрерывный луч.
-        public string SecondPerkName { get; set; } //Самая долгая зима
-        public string SecondPerkDescription { get; set; } //Чем дольше лазер Холодного сердца наведен на цель, тем больший урон он наносит.
-        public string DropLocation { get; set; } //Экзотические энграммы, редкие выпадения в открытом мире.
-        public int Catalyst { get; set; } // 0 - Отсутствует. 1 - Есть.
-        public string WhereCatalystDrop { get; set; } // Убить босса в героическом или Сумрачном налете.
-        public string CatalystQuest { get; set; }// Убить 300 монстров в любой точке солнечной системы.
-        public string CatalystBonus { get; set; } // Стабильность +20 и перезарядка +20.
+	public class Gear
+	{
+		[Key]
+		public int Id { get; set; }
+		[Display(Name = "Название")]
+		public string Name { get; set; } //Холодное сердце
+		[Display(Name = "Тип")]
+		public string Type { get; set; } //Энергетическая лучевая винтовка
+		[Display(Name = "Иконка")]
+		public string IconUrl { get; set; } //http://159.69.21.188/Icon/Energy/Coldheart.jpg
+		[Display(Name = "Скриншот")]
+		public string ImageUrl { get; set; } //http://159.69.21.188/Image/Energy/Coldheart.jpg
+		[Display(Name = "Описание")]
+		public string Description { get; set; } //Новейшие технологии Omolon позволяют использовать жидкое топливо в качестве хладагента, обеспечивая оружейным системам минусовую температуру.
+		[Display(Name = "Основной перк")]
+		public string PerkName { get; set; } //Холодный синтез
+		[Display(Name = "Описание перка")]
+		public string PerkDescription { get; set; } //Это оружие оснащено лазером с термоядерной накачкой, испускающим непрерывный луч.
+		[Display(Name = "Вторичный перк")]
+		public string SecondPerkName { get; set; } //Самая долгая зима
+		[Display(Name = "Описание перка")]
+		public string SecondPerkDescription { get; set; } //Чем дольше лазер Холодного сердца наведен на цель, тем больший урон он наносит.
+		[Display(Name = "Как получить")]
+		public string DropLocation { get; set; } //Экзотические энграммы, редкие выпадения в открытом мире.
+		[Display(Name = "Катализатор")]
+		public int Catalyst { get; set; } // 0 - Отсутствует. 1 - Есть.
+		[Display(Name = "Как получить катализатор")]
+		public string WhereCatalystDrop { get; set; } // Убить босса в героическом или Сумрачном налете.
+		[Display(Name = "Задание катализатора")]
+		public string CatalystQuest { get; set; }// Убить 300 монстров в любой точке солнечной системы.
+		[Display(Name = "Бонус катализатора")]
+		public string CatalystBonus { get; set; } // Стабильность +20 и перезарядка +20.
 
-    }
+	}
 }
--- Web/Controllers/GearsController.cs
+++ Web/Controllers/GearsController.cs
@@ -9,145 +9,155 @@ using Core.Models.Db;
 
 namespace Web.Controllers
 {
-    public class GearsController : Controller
-    {
-        private readonly FailsafeContext _context;
+	public class GearsController : Controller
+	{
+		private readonly FailsafeContext _context;
 
-        public GearsController(FailsafeContext context)
-        {
-            _context = context;
-        }
+		public GearsController(FailsafeContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Gears
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Gears.ToListAsync());
-        }
+		// GET: Gears
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Gears.ToListAsync());
+		}
 
-        // GET: Gears/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Gears/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var gear = await _context.Gears
+			var gear = await _context.Gears
 				.AsNoTracking()
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (gear == null)
-            {
-                return NotFound();
-            }
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (gear == null)
+			{
+				return NotFound();
+			}
 
-            return View(gear);
-        }
+			return View(gear);
+		}
 
-        // GET: Gears/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Gears/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Gears/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Type,IconUrl,ImageUrl,Description,PerkName,PerkDescription,SecondPerkName,SecondPerkDescription,DropLocation,Catalyst,WhereCatalystDrop,CatalystQuest,CatalystBonus")] Gear gear)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(gear);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(gear);
-        }
+		// POST: Gears/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Name,Type,IconUrl,ImageUrl,Description,PerkName,PerkDescription,SecondPerkName,SecondPerkDescription,DropLocation,Catalyst,WhereCatalystDrop,CatalystQuest,CatalystBonus")] Gear gear)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					_context.Add(gear);
+					await _context.SaveChangesAsync();
+					return RedirectToAction(nameof(Index));
+				}
+			}
+			catch (DbUpdateException /* ex */)
+			{
+				//Логгирование ошибки (Раскомментируй переменную ex и реализуй логгирование).
+				ModelState.AddModelError("", "Неудалось сохранить изменения. " +
+					"Попробуй немного позже, если ошибка все еще будет " +
+					"сообщи администратору.");
+			}
+			return View(gear);
+		}
 
-        // GET: Gears/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Gears/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var gear = await _context.Gears.FindAsync(id);
-            if (gear == null)
-            {
-                return NotFound();
-            }
-            return View(gear);
-        }
+			var gear = await _context.Gears.FindAsync(id);
+			if (gear == null)
+			{
+				return NotFound();
+			}
+			return View(gear);
+		}
 
-        // POST: Gears/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Type,IconUrl,ImageUrl,Description,PerkName,PerkDescription,SecondPerkName,SecondPerkDescription,DropLocation,Catalyst,WhereCatalystDrop,CatalystQuest,CatalystBonus")] Gear gear)
-        {
-            if (id != gear.Id)
-            {
-                return NotFound();
-            }
+		// POST: Gears/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Type,IconUrl,ImageUrl,Description,PerkName,PerkDescription,SecondPerkName,SecondPerkDescription,DropLocation,Catalyst,WhereCatalystDrop,CatalystQuest,CatalystBonus")] Gear gear)
+		{
+			if (id != gear.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(gear);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GearExists(gear.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(gear);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(gear);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!GearExists(gear.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(gear);
+		}
 
-        // GET: Gears/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Gears/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var gear = await _context.Gears
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (gear == null)
-            {
-                return NotFound();
-            }
+			var gear = await _context.Gears
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (gear == null)
+			{
+				return NotFound();
+			}
 
-            return View(gear);
-        }
+			return View(gear);
+		}
 
-        // POST: Gears/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var gear = await _context.Gears.FindAsync(id);
-            _context.Gears.Remove(gear);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: Gears/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var gear = await _context.Gears.FindAsync(id);
+			_context.Gears.Remove(gear);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool GearExists(int id)
-        {
-            return _context.Gears.Any(e => e.Id == id);
-        }
-    }
+		private bool GearExists(int id)
+		{
+			return _context.Gears.Any(e => e.Id == id);
+		}
+	}
 }
