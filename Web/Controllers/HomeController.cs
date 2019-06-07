97fd3f6178601292201fb1fa1c93ba4a13ceaaab
Cyber-Garret <cyber.garret@outlook.com>
2019-06-07 17:14:51 +0300

    WIP: Update CSS style

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -35,6 +35,17 @@ namespace Web.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+
+		[Route("AddMe")]
+		public IActionResult Add_Me()
+		{
+			return RedirectPermanent(@"https://discordapp.com/oauth2/authorize?client_id=521693707238506498&scope=bot&permissions=8");
+		}
+		[Route("JoinMe")]
+		public IActionResult Join_Me()
+		{
+			return RedirectPermanent(@"https://discord.gg/WcuNPM9");
+		}
 		#endregion
 	}
 }
