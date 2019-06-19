16c514a4ee143ec3baeedc02b3ac0dd5ba46049a
Cyber-Garret <cyber.garret@outlook.com>
2019-06-19 16:05:46 +0300

    WIP Bungie OAuth

---
+++ Bungie.OAuth2/BungieExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Bungie.OAuth2;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class BungieExtensions
+	{
+		public static AuthenticationBuilder AddBungie(this AuthenticationBuilder builder)
+		  => builder.AddBungie(BungieDefaults.AuthenticationScheme, _ => { });
+
+		public static AuthenticationBuilder AddBungie(this AuthenticationBuilder builder, Action<BungieOptions> configureOptions)
+			=> builder.AddBungie(BungieDefaults.AuthenticationScheme, configureOptions);
+
+		public static AuthenticationBuilder AddBungie(this AuthenticationBuilder builder, string authenticationScheme, Action<BungieOptions> configureOptions)
+			=> builder.AddBungie(authenticationScheme, BungieDefaults.DisplayName, configureOptions);
+
+		public static AuthenticationBuilder AddBungie(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<BungieOptions> configureOptions)
+			=> builder.AddOAuth<BungieOptions, BungieHandler>(authenticationScheme, displayName, configureOptions);
+	}
+}
