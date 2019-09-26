fcd0a7efa3797e737148d231428f5a4c30380216
Garret <cyber.garret@outlook.com>
2019-09-26 16:55:01 +0300

    WIP on website + Bot UI control panel

---
+++ Neira.Bot.UI/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Neira.Bot.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Neira.Bot.UI
+{
+	public class ApplicationContext : DbContext
+	{
+		public DbSet<Clan> Clans { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder builder)
+		{
+			var connection = new SqlConnectionStringBuilder
+			{
+				DataSource = "ip",
+				InitialCatalog = "db",
+				UserID = "login",
+				Password = "password",
+				MultipleActiveResultSets = true
+			};
+
+			builder.UseSqlServer(connection.ConnectionString);
+		}
+	}
+}
---
+++ Neira.Bot.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neira.Bot.UI.ViewModel
+{
+	public class MainViewModel
+	{
+
+	}
+}
