f2863131da42d44d92f6a298422e757ba4bcbbe7
Garret <cyber.garret@outlook.com>
2019-11-05 10:13:56 +0200

    done move web project to MVC

--- Neira.MVC/Controllers/HomeController.cs
+++ Neira.MVC/Controllers/HomeController.cs
@@ -32,6 +32,24 @@ namespace Neira.MVC.Controllers
 			return View(model);
 		}
 
+		[Route("AddBot")]
+		public IActionResult AddBot() => RedirectPermanent($"https://discordapp.com/oauth2/authorize?client_id=521693707238506498&scope=bot&permissions=269479104");
+
+		[Route("YandexMoney")]
+		public IActionResult YandexMoney() => RedirectPermanent($"https://money.yandex.ru/to/410019748161790");
+
+		[Route("Patreon")]
+		public IActionResult Patreon() => RedirectPermanent($"https://www.patreon.com/Cyber_Garret");
+
+		[Route("BlackExodus")]
+		public IActionResult BlackExodus() => RedirectPermanent($"https://discord.gg/WcuNPM9");
+
+		[Route("VKGroup")]
+		public IActionResult VkGroup() => RedirectPermanent($"https://vk.com/failsafe_bot");
+
+		[Route("MyGithub")]
+		public IActionResult Github() => RedirectPermanent($"https://github.com/Cyber-Garret");
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
