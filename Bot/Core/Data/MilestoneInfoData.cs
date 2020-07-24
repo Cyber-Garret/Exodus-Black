32efdf9b750d889fa3cf70e8c741357e05bd8a3c
Cyber_Garret <garret333@gmail.com>
2020-07-24 16:30:46 +0300

    wip: milestone locale fix

--- Bot/Core/Data/MilestoneInfoData.cs
+++ Bot/Core/Data/MilestoneInfoData.cs
@@ -24,6 +24,11 @@ namespace Bot.Core.Data
 			return MilestonesData.Values.Where(m => m.MilestoneType == milestoneType).ToList();
 		}
 
+		/// <summary>
+		/// Search any Milestone info with type
+		/// </summary>
+		/// <param name="name">milestone alias</param>
+		/// <returns></returns>
 		internal static MilestoneInfo SearchMilestoneData(string name, MilestoneType type)
 		{
 			var searchName = MilestonesData.Keys.FirstOrDefault(key => key.Contains(name, StringComparison.InvariantCultureIgnoreCase));
@@ -35,6 +40,23 @@ namespace Bot.Core.Data
 					return milestone;
 				else
 					return null;
+			}
+			else
+				return null;
+		}
+
+		/// <summary>
+		/// Search any Milestone info without type
+		/// </summary>
+		/// <param name="name">milestone alias</param>
+		/// <returns></returns>
+		internal static MilestoneInfo SearchMilestoneData(string name)
+		{
+			var searchName = MilestonesData.Keys.FirstOrDefault(key => key.Contains(name, StringComparison.InvariantCultureIgnoreCase));
+			if (searchName != null)
+			{
+				MilestonesData.TryGetValue(searchName, out var milestone);
+				return milestone;
 			}
 			else
 				return null;
