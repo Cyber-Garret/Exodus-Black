c6a504b84156a971e63afdc6cb05de46eda62a08
RA <ra@jooble.com>
2021-07-27 12:25:59 +0300

    added gefourcenow promo, restored gtag

---
+++ Ex-077Command/Controllers/LinkController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex077.Controllers
+{
+	public class LinkController : Controller
+	{
+		[Route("GeForceNow")]
+		public IActionResult GeforceNow() =>
+			RedirectPermanent("https://ad.admitad.com/g/wxfnqla9g4846e6131458cd5089896/");
+	}
+}
